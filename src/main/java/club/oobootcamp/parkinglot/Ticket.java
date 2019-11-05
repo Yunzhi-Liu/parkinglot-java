db831e541182ba96a3d9eb0ad509856649a237b6
Yunzhi-Liu <yzliu@thoughtworks.com>
2019-11-05 20:56:11 +0800

    refactor(ticket): override equals and hashcode methods

--- src/main/java/club/oobootcamp/parkinglot/Ticket.java
+++ src/main/java/club/oobootcamp/parkinglot/Ticket.java
@@ -1,4 +1,30 @@
 package club.oobootcamp.parkinglot;
 
+import java.util.UUID;
+
 public class Ticket {
+
+    private final UUID id;
+
+    public Ticket() {
+        this.id = UUID.randomUUID();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Ticket)) {
+            return false;
+        }
+        final Ticket ticket = (Ticket) o;
+
+        return id.equals(ticket.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
